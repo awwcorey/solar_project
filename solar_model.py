32253603cf287c3a6e3a7d91dc253d15788c7531
awwcorey <awwcorey@net123.ipoe-users.mipt.ru>
2016-11-10 15:36:12 +0300

    исправила физику вроде

--- solar_model.py
+++ solar_model.py
@@ -31,15 +31,11 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx / body.m
-
-    body.x += (1 / 2) * ax * (dt ** 2) + body.Vx * dt
-    body.Vx += ax * dt
-    # FIXME: not done recalculation of y coordinate!
-
-    ay = body.Fy / body.m
-
-    body.y += (1 / 2) * ay * (dt ** 2) + body.Vy * dt
+    ax = body.Fx/body.m
+    body.x += body.Vx*dt +(ax*(dt)**2)/2
+    body.Vx += ax*dt
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt +(ay*(dt)**2)/2
     body.Vy += ay*dt
 
 
